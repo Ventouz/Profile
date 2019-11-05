2cca62f7a9102f9ccc39fe161cc7b60117092f79
Ventouz <potoroo725@gmail.com>
2019-11-05 20:32:23 +0100

    ajout bmon au setup

--- setup.sh
+++ setup.sh
@@ -4,6 +4,22 @@ cp *.tff /usr/share/fonts/
 cp ./prettyping /usr/bin/prettyping
 
 apt install -y fd hyperfine htop ranger git
+
+cd ~
+
+## bat - a cat clone with wings
 wget https://github.com/sharkdp/bat/releases/download/v0.12.1/bat_0.12.1_amd64.deb && dpkg -i bat_*.deb
+
+## lsd - ls on steroids
 wget https://github.com/Peltoche/lsd/releases/download/0.16.0/lsd-musl_0.16.0_amd64.deb && dpkg -i lsd*.deb
+
+## bmon - network interfaces monitoring
+git clone https://github.com/tgraf/bmon.git ~/bmon
+cd ~/bmon
+apt-get install build-essential make libconfuse-dev libnl-3-dev libnl-route-3-dev libncurses-dev pkg-config dh-autoreconf
+./autogen.sh
+./configure
+make
+make install
+
 
