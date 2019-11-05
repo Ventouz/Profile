51cae638e4a972723b36b77ebebfb5dd64e50966
Ventouz <potoroo725@gmail.com>
2019-11-05 19:53:12 +0100

    install ssh keys

--- apply.sh
+++ apply.sh
@@ -3,6 +3,6 @@ cp ./bash_profile ~/.bash_profile
 cp ./bashrc ~/.bashrc
 
 mkdir ~/.ssh/
-cat authorized_keys >> authorized_keys
+cat authorized_keys >> ~/.ssh/authorized_keys
 
 source ~/.bash_profile
