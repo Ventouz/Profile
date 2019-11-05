0769f1fe01c0d4b320f1442e6423cde6e35ab2d3
Ventouz <potoroo725@gmail.com>
2019-11-05 19:55:39 +0100

    install lsd

--- setup.sh
+++ setup.sh
@@ -3,5 +3,7 @@
 cp *.tff /usr/share/fonts/
 cp ./prettyping /usr/bin/prettyping
 
-apt install -y lsd fd hyperfine htop ranger git
+apt install -y fd hyperfine htop ranger git
 wget https://github.com/sharkdp/bat/releases/download/v0.12.1/bat_0.12.1_amd64.deb && dpkg -i bat_*.deb
+wget https://github.com/Peltoche/lsd/releases/download/0.16.0/lsd-musl_0.16.0_amd64.deb && dpkg -i lsd*.deb
+
