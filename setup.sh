5d862bbe7f5016b1f2248b11792927cf9dd5526c
Ventouz <potoroo725@gmail.com>
2020-03-21 00:17:31 +0100

    bon lien prettyping

--- setup.sh
+++ setup.sh
@@ -11,6 +11,6 @@ wget -qO- https://raw.githubusercontent.com/Ventouz/Profile/master/motd/0-clear 
 wget -qO- https://raw.githubusercontent.com/Ventouz/Profile/master/motd/10-uname > /etc/update-motd.d/10-uname
 wget -qO- https://raw.githubusercontent.com/Ventouz/Profile/master/motd/20-screenfetch > /etc/update-motd.d/20-screenfetch
 wget -qO- https://raw.githubusercontent.com/Ventouz/Profile/master/motd/30-sysinfo > /etc/update-motd.d/30-sysinfo
-wget -qO- https://github.com/denilsonsa/prettyping/blob/master/prettyping > ~/.prettyping
+wget -qO- https://raw.githubusercontent.com/denilsonsa/prettyping/master/prettyping > ~/.prettyping
 chmod +x ~/.prettyping
 rm -f /etc/motd
