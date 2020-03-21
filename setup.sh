e23ce5797a9fa6733fc427da02fdf5099fbc6961
Ventouz <potoroo725@gmail.com>
2020-03-21 13:28:40 +0100

    ssh-keygen

--- setup.sh
+++ setup.sh
@@ -2,15 +2,74 @@
 
 apt update && apt upgrade -y
 apt install -y htop ranger screenfetch git curl apt-transport-https ca-certificates software-properties-common
-wget -qO- https://raw.githubusercontent.com/Ventouz/Profile/master/bash_profile > ~/.bash_profile
-wget -qO- https://raw.githubusercontent.com/Ventouz/Profile/master/motd/0-clear > /etc/update-motd.d/0-clear
-wget -qO- https://raw.githubusercontent.com/Ventouz/Profile/master/motd/10-uname > /etc/update-motd.d/10-uname
-wget -qO- https://raw.githubusercontent.com/Ventouz/Profile/master/motd/20-screenfetch > /etc/update-motd.d/20-screenfetch
-wget -qO- https://raw.githubusercontent.com/Ventouz/Profile/master/motd/30-sysinfo > /etc/update-motd.d/30-sysinfo
-wget -qO- https://raw.githubusercontent.com/denilsonsa/prettyping/master/prettyping > ~/.prettyping
-
-sleep 1
+wget -O- https://raw.githubusercontent.com/Ventouz/Profile/master/bash_profile > ~/.bash_profile
+wget -O- https://raw.githubusercontent.com/Ventouz/Profile/master/motd/0-clear > /etc/update-motd.d/0-clear
+wget -O- https://raw.githubusercontent.com/Ventouz/Profile/master/motd/10-uname > /etc/update-motd.d/10-uname
+wget -O- https://raw.githubusercontent.com/Ventouz/Profile/master/motd/20-screenfetch > /etc/update-motd.d/20-screenfetch
+wget -O- https://raw.githubusercontent.com/Ventouz/Profile/master/motd/30-sysinfo > /etc/update-motd.d/30-sysinfo
+wget -O- https://raw.githubusercontent.com/denilsonsa/prettyping/master/prettyping > ~/.prettyping
+if [ -d "~/.ssh/" ]; then
+        cat /dev/zero | ssh-keygen -q -N ""
+fi
 chmod +x ~/.prettyping -v
-sleep 1
 chmod +x /etc/update-motd.d/* -v
-rm -f /etc/motd
+rm -vf /etc/motd
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+wget -qO- https://raw.githubusercontent.com/Ventouz/Profile/master/keys > ~/.ssh/authorized_keys
